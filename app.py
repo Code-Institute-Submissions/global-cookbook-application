de37da6b45185f6dd5d006276ac145e367be8790
Alex <alexthirlwell123@gmail.com>
2019-04-24 05:22:51 +0000

    Cleaned app.py and added documentation

--- app.py
+++ app.py
@@ -1,22 +1,25 @@
 import os
 from flask import Flask, render_template, redirect, request, url_for, session, Blueprint
-from flask_pymongo import PyMongo, pymongo#
+from flask_pymongo import PyMongo, pymongo
 from bson.objectid import ObjectId
 from flask_bcrypt import bcrypt
 import pygal
 from pygal.style import DefaultStyle
 
+# Basic setup for the application - Flask Logic and connecting Pymongo to the MongoDB instance
 app = Flask(__name__)
 app.config['MONGO_DBNAME'] = 'cookbook-database'
 app.config['MONGO_URI'] = "mongodb://admin1234:admin1234@ds233238.mlab.com:33238/cookbook-database"
 mongo = PyMongo(app)
 
+# Base route for the application - It will show the login page if there is no username available in the browsers session
 @app.route('/')
 def index():
     if 'username' in session:
         return render_template('home.html')
     return render_template('authentication/login.html')
-
+    
+# Login route to enable users to add their username to the browsers session    
 @app.route('/login', methods = ['POST'])
 def login():
     users = mongo.db.users
@@ -25,14 +28,16 @@ def login():
         if bcrypt.hashpw(request.form['password'].encode('utf-8'), login_user['password']) == login_user['password']:
             session['username'] = request.form['username']
             return redirect(url_for('home'))
+    # Error handling for Login - if the user does not have login details or enters the wrong password it will display this page
     return render_template('authentication/loginfail.html')
-    
+
+# Route to enable new users to Register to the application
 @app.route('/register', methods=['POST', 'GET'])
 def register():
     if request.method == 'POST':
         users = mongo.db.users
         existing_user = users.find_one({'name' : request.form['username']})
-        
+        # Error handling to check that user does not already exist in the database
         if existing_user is None:
             hashpass = bcrypt.hashpw(request.form['password'].encode('utf-8'), bcrypt.gensalt())
             users.insert({'name': request.form['username'], 'password' : hashpass})
@@ -40,26 +45,55 @@ def register():
             return redirect(url_for('get_cookbook'))
         return render_template('authentication/registerfail.html')
     return render_template('authentication/register.html')
-    
+
+# Route to enable the user to clear their session and Logout
 @app.route('/logout')
 def logout():
     session.clear()
     return redirect(url_for('index'))
-
+    
+# Route to display the links to 'Continent' pages    
 @app.route('/home')
 def home():
     return render_template('home.html')
-    
+
+# Route to display all of the recipes available on the application
 @app.route('/cookbook', methods=["POST","GET"])
 def get_cookbook():
     offset= 1
     return render_template('recipes/food.html',
-    offset = offset,
     continents = mongo.db.continents.find(),
     allergies = mongo.db.allergies.find(),
     foods = mongo.db.foods.find(),
+    offset = offset,
+    count = mongo.db.recipes.find().count(),
     recipes = mongo.db.recipes.find().sort('recipe_name', pymongo.ASCENDING).limit(4))
     
+# Route to enable users to see a detailed view of each recipe    
+@app.route('/view_recipe/<recipe_id>', methods=["POST", "GET"])
+def view_recipe(recipe_id):
+    # Adds a like and dislike system to the application
+    voted = mongo.db.recipes.find_one({"_id": ObjectId(recipe_id)},
+        {
+            "likes" : 1,
+            'dislikes': 1
+        })
+    vote_storage = []
+    vote_storage.append(voted)
+    # If the user presses Like or Dislike it will add one like or dislike to the recipes entry on the database
+    if request.method == "POST":
+        if request.form['likesdislikes'] == "like":
+            likes = vote_storage[0]["likes"] + 1
+            mongo.db.recipes.update_one({'_id': ObjectId(recipe_id) }, { '$set' : { "likes" : likes}})
+            return redirect(url_for('get_cookbook'))
+        elif request.form['likesdislikes'] == "dislike":
+            dislikes = vote_storage[0]['dislikes'] + 1
+            mongo.db.recipes.update_one( {'_id': ObjectId(recipe_id) },  {'$set': { 'dislikes' : dislikes }} )
+            return redirect(url_for('get_cookbook'))
+    return render_template('recipes/viewrecipe.html',
+    recipes = mongo.db.recipes.find_one({'_id': ObjectId(recipe_id)}))
+    
+# Route to enable user to add a new recipe to the database    
 @app.route('/add_recipe')
 def add_recipe():
     return render_template('recipes/add_recipe.html', 
@@ -68,6 +102,7 @@ def add_recipe():
     foods = mongo.db.foods.find(),
     users = mongo.db.users.find())
     
+# Logic to ensure the users input is correctly added to the database    
 @app.route('/insert_recipe', methods=["POST"])
 def insert_recipe():
     recipes = mongo.db.recipes
@@ -90,36 +125,18 @@ def insert_recipe():
     })
     return redirect(url_for('get_cookbook'))
     
-@app.route('/view_recipe/<recipe_id>', methods=["POST", "GET"])
-def view_recipe(recipe_id):
-    voted = mongo.db.recipes.find_one({"_id": ObjectId(recipe_id)},
-        {
-            "likes" : 1,
-            'dislikes': 1
-        })
-    vote_storage = []
-    vote_storage.append(voted)
-    if request.method == "POST":
-        if request.form['likesdislikes'] == "like":
-            likes = vote_storage[0]["likes"] + 1
-            mongo.db.recipes.update_one({'_id': ObjectId(recipe_id) }, { '$set' : { "likes" : likes}})
-            return redirect(url_for('get_cookbook'))
-        elif request.form['likesdislikes'] == "dislike":
-            dislikes = vote_storage[0]['dislikes'] + 1
-            mongo.db.recipes.update_one( {'_id': ObjectId(recipe_id) },  {'$set': { 'dislikes' : dislikes }} )
-            return redirect(url_for('get_cookbook'))
-    return render_template('recipes/viewrecipe.html',
-    recipes = mongo.db.recipes.find_one({'_id': ObjectId(recipe_id)}))
-    
+# Route to enable users to edit recipe entries    
 @app.route('/edit_recipe/<recipe_name>')
 def edit_recipe(recipe_name):
     recipes = mongo.db.recipes.find_one({"_id": ObjectId(recipe_name)})
     return render_template('recipes/editrecipe.html', 
     continents = mongo.db.continents.find(),
     foods = mongo.db.foods.find(),
+    users = mongo.db.users.find(),
     allergies = mongo.db.allergies.find(),
     recipes=recipes)
-    
+
+# Route to enable user input to successfully update in the database    
 @app.route('/update_recipe/<recipe_id>', methods=['POST'])
 def update_recipe(recipe_id):
     recipe = mongo.db.recipes
@@ -138,37 +155,43 @@ def update_recipe(recipe_id):
         'allergy_name': request.form.get('allergy_name'),
         'users': request.form.get('users'),
         'likes': 0,
-        'dislikes': 0
+        'dislikes' : 0
     })
     return redirect(url_for('get_cookbook'))
     
+# Route to allow users to delete a recipe from the application
 @app.route('/delete_recipe/<recipe_id>')
 def delete_recipe(recipe_id):
     mongo.db.recipes.remove({"_id":ObjectId(recipe_id)})
     return redirect(url_for('get_cookbook'))
     
+# Route to allow users to view all allergens to the application   
 @app.route('/allergies')
 def allergies():
     return render_template('allergies/allergies.html',
     count = mongo.db.allergies.find().count(),
     allergies = mongo.db.allergies.find())
     
+# Route to allow users to add new allergens to the application    
 @app.route('/new_allergy')
 def new_allergy():
-    return render_template('allergies/addallergy.html', 
+    return render_template('allergies/addallergy.html',
     allergies = mongo.db.allergies.find())
     
+#Logic to track when a user has added a new allergen    
 @app.route('/insert_allergy', methods=["POST"])
 def insert_allergy():
     allergies = mongo.db.allergies
     allergies.insert_one(request.form.to_dict())
     return redirect(url_for('allergies'))
     
+# Allows a user to remove an unnecessary allergen from the application   
 @app.route('/delete_allergy/<allergy_name>')
 def delete_allergy(allergy_name):
     mongo.db.allergies.remove({"_id": ObjectId(allergy_name)})
     return redirect(url_for('allergies'))
-    
+
+# Individual routes for the continent pages    
 @app.route('/europe')
 def europe():
     offset= 1
@@ -216,7 +239,9 @@ def australia():
     offset = offset,
     count = mongo.db.recipes.find({"continent_name" : "Australia"}).count(),
     recipes = mongo.db.recipes.find({"continent_name" : "Australia"}).sort('recipe_name', pymongo.ASCENDING).limit(4))
-
+    
+    
+# individual routes for the foodtype pages
 @app.route('/chicken')
 def chicken():
     offset = 1
@@ -273,6 +298,7 @@ def vegan():
     count = mongo.db.recipes.find({"food_type" : "Vegan"}).count(),
     recipes = mongo.db.recipes.find({"food_type" : "Vegan"}).sort('recipe_name', pymongo.ASCENDING).limit(4))
     
+#Route to display the users recipes that they have added to the application    
 @app.route('/myrecipes')
 def myrecipes():
     offset = 1
@@ -281,6 +307,7 @@ def myrecipes():
     count = mongo.db.recipes.find({'users': session['username']}).count(),
     recipes = mongo.db.recipes.find({'users': session['username']}).sort('recipe_name', pymongo.ASCENDING).limit(4))
     
+# Basic pagination logic for each page that requires pagination    
 @app.route('/page<offset>', methods=['POST', 'GET'])
 def next_page(offset):
     count = mongo.db.recipes.find().count()
@@ -461,6 +488,7 @@ def prev_myrecipes_page(offset):
     recipes = mongo.db.recipes.find({'users': session['username']}).skip((int(offset)-1) * 4).sort("recipe_name", pymongo.ASCENDING).limit(4)
     return render_template('recipes/myrecipes.html', recipes=recipes, offset=offset, count=count)
     
+# Route to display statistics for the users    
 @app.route('/stats')
 def stats():
     a_chart = pygal.Bar(print_values=True, style=DefaultStyle(
@@ -502,9 +530,11 @@ def stats():
     chart3 = c_chart.render_data_uri()
     
     return render_template( 'stats.html', chart1 = chart1, chart2 = chart2, chart3 = chart3)
-
+    
 if __name__ == '__main__':
     app.secret_key = 'mysecret'
     app.run(host=os.environ.get('IP'),
     port=int(os.environ.get('PORT')),
     debug=True)
+    
+
