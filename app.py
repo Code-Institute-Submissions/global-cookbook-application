10ea0e00c0082bba162c36a52fcb58a6cfb56e69
Alex <alexthirlwell123@gmail.com>
2019-04-24 04:25:29 +0000

    added in login page for authentication

--- app.py
+++ app.py
@@ -2,11 +2,22 @@ import os
 from flask import Flask, render_template, redirect, request, url_for, session, Blueprint
 from flask_pymongo import PyMongo, pymongo#
 from bson.objectid import ObjectId
+from flask_bcrypt import bcrypt
 
 app = Flask(__name__)
 app.config['MONGO_DBNAME'] = 'cookbook-database'
 app.config['MONGO_URI'] = "mongodb://admin1234:admin1234@ds233238.mlab.com:33238/cookbook-database"
 mongo = PyMongo(app)
+
+@app.route('/login', methods = ['POST'])
+def login():
+    users = mongo.db.users
+    login_user = users.find_one({'name' : request.form['username']})
+    if login_user:
+        if bcrypt.hashpw(request.form['password'].encode('utf-8'), login_user['password']) == login_user['password']:
+            session['username'] = request.form['username']
+            return redirect(url_for('home'))
+    return render_template('authentication/loginfail.html')
 
 @app.route('/home')
 def home():
